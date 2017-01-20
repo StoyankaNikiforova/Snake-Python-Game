82868db58d9de8ec2161f9beb5eb2ed1df57e3ae
StoyankaNikiforova <tanija@abv.bg>
2017-01-20 07:23:53 +0200

    Fix mixin atr

--- class_cell.py
+++ class_cell.py
@@ -2,8 +2,8 @@ from class_world_object import WorldObject
 from class_vector2D import Vector2D
 
 
-class Cell(WorldObject, Vector2D):
-    def __init__(self, x, y,  contents=None,):
+class Cell(Vector2D, WorldObject):
+    def __init__(self, x, y,  contents=None):
         super().__init__(x, y)
         self.contents = contents
 
